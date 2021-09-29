700da60c575b788f1180b34e62be247d72d4e732
Liwen Xia <liwenxia66@gmail.com>
2021-09-29 15:19:58 -0400

    compiling print_functions.c

--- homework/hw3/print_functions.c
+++ homework/hw3/print_functions.c
@@ -12,37 +12,49 @@
 void printMaze(char maze[][200], const int maze_width, const int maze_height) {
   // TODO: implement this function
   printf("Maze: %d, %d", maze_width, maze_height);
-  for(int r = 0; r <maze_height, ++r){
-    for(int c = 0; c < maze_width, ++c){
+  for(int r = 0; r <maze_height; r++){
+    for(int c = 0; c < maze_width; c++){
       if(c==maze_width-1){
 	printf("%c\n",maze[r][c]);
       }
       else{
 	printf("%c",maze[r][c]);}
+    }
+  }
 }
 
 // The function to print the maze's solution
 void printSolution(char maze[][200], const int maze_width, const int maze_height, char sol[][200]) {
   // TODO: implement this function
-printf("Solution path (*):");
-  for(int r = 0; r <maze_height, ++r){
-    for(int c = 0; c < maze_width, ++c){
-      if(maze[r][c]==" "&&sol[r][c]!=" "){
+  
+printf("Solution path (*):\n");
+ for(int r = 0; r <maze_height; ++r){
+   for(int c = 0; c < maze_width; ++c){
+      if(sol[r][c]=='*'){
 	if(c==maze_width-1){
         printf("%c\n",sol[r][c]);
-      }
-      else{
-        printf("%c",sol[r][c]);}
-      }
-      else{
-      if(c==maze_width-1){
+        }
+        else{
+          printf("%c",sol[r][c]);}
+        }  
+      else
+	{
+	  if(c==maze_width-1){
         printf("%c\n",maze[r][c]);
-      }
-      else{
-        printf("%c",maze[r][c]);}}
+        }
+        else{
+          printf("%c",maze[r][c]);}
+   }
+   }
+ }
 }
-
 // The function to print the error message
 void printError(const int return_code, char* argv[]) {
   // TODO: implement this function
+  if(return_code==1){
+    printf("No solution found for the input maze");}
+  if(return_code==2){
+    printf("Generate maze usage: ./hw3 <maze file> <width> <height> [threshold = 0.5] [seed = 0]\nSolve maze usage: ./hw3 <maze file>");}
+  if(return_code==-1){
+    printf("Cannot open %s to read",argv[1]);}
 }
