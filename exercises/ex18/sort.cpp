9fee8d78bce31b59e9b6cc124668cec96233f97a
Liwen Xia <liwenxia66@gmail.com>
2021-10-25 10:23:25 -0400

    compiling version

--- exercises/ex18/sort.cpp
+++ exercises/ex18/sort.cpp
@@ -30,17 +30,16 @@ void sort( std::vector< int > *values )
     int mid = values->size()/2;
     std::vector< int > left(mid);
     std::vector< int > right(values->size()-mid);
-    for(int i = 0; i < mid; i++) left[i] = (*values)[i];
-    for(int i = mid; i < (int)values->size(); i++) right[i-mid]=(*values)[i];
+    for(int i = 0; i < mid; i++) {left[i] = (*values)[i];}
+    for(int i = mid; i < (int)values->size(); i++) {right[i-mid]=(*values)[i];}
     sort( &left);
     sort( &right);
-    size_t idx = 0; i = 0; j = 0;
+    size_t idx = 0, i = 0, j = 0;
     while(i<left.size() || j <right.size()){
-      if(i>=left.size()) (*values)[idx++]=right[j++];
-      else if(j>=right.size()) (*values)[idx++] = left[i++];
-      else if (left[i]<=right[j] (*values)[idx++] = left[i++];
-	       else (*values)[idx++] = right[j++];
+      if(i>=left.size()) {(*values)[idx++]=right[j++];}
+      else if(j>=right.size()) {(*values)[idx++] = left[i++];}
+      else if (left[i]<=right[j]) {(*values)[idx++] = left[i++];}
+	       else {(*values)[idx++] = right[j++];}
     }
   }
 }
-
